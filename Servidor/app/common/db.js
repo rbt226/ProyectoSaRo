24c9d847a1a459ba9aff1c5d606a736d313d5696
robert <robert226@gmail.com>
2020-04-12 22:25:39 -0300

    db config

--- Servidor/app/common/db.js
+++ Servidor/app/common/db.js
@@ -1,13 +1,9 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "sql10.freesqldatabase.com",
-  user: "sql10332853",
-  password: "BaIqtHsbXN",
-  database: "sql10332853",
-});
+//get config db file 
+var config = require('./config.json');
+var connection = mysql.createConnection(config);
 
-//Port: 3306
 connection.connect(function (err) {
   if (err) {
     console.error("Error connecting: " + err.stack);
