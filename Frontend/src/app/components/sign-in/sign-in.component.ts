2f875e476aa037311f3f5a98b2cfae2fc0caa0b2
Sabrina Sosa <ssosa@technisys.com>
2020-05-07 23:06:58 -0300

    Sign In identacion

--- Frontend/src/app/components/sign-in/sign-in.component.ts
+++ Frontend/src/app/components/sign-in/sign-in.component.ts
@@ -1,41 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AppComponent } from 'src/app/app.component';
 import { AuthService } from 'src/app/services/auth.service';
+import { ModalService } from 'src/app/services/modal.service';
 import { SpinnerService } from 'src/app/services/spinner.service';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss'],
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-  constructor(
-    private autService: AuthService,
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private spinnerService: SpinnerService,
-    private appComponent: AppComponent
-  ) {}
+    constructor(
+        private autService: AuthService,
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private spinnerService: SpinnerService,
+        private modalService: ModalService
+    ) { }
 
-  formSignIn: FormGroup;
-  formSubmitted = false;
+    formSignIn: FormGroup;
 
-  ngOnInit() {
-    this.formSignIn = this.formBuilder.group({
-      mail: ['', [Validators.required,  Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
-      password: ['', Validators.required],
-    });
-  }
-
-  get form() {
-    return this.formSignIn.controls;
-  }
-
-  signIn(data) {
-    event.preventDefault();
-    this.formSubmitted = true;
+    ngOnInit() {
+        this.formSignIn = this.formBuilder.group({
+            mail: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+        });
+    }
 
     if (this.formSignIn.valid) {
       
