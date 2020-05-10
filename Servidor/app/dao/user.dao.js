ad12f91ee9160fe860756d23ec9032377624372a
Sabrina Sosa <ssosa@technisys.com>
2020-05-10 04:43:03 -0300

    Se resuelven conflictos.

--- Servidor/app/dao/user.dao.js
+++ Servidor/app/dao/user.dao.js
@@ -2,7 +2,6 @@ const userModel = require("../models/user.model");
 const utils = require("../common/utils");
 
 exports.signIn = (email, password, result) => {
-<<<<<<< HEAD
     userModel
         .findOne({ where: { email: email } })
         .then((us) => {
@@ -17,20 +16,6 @@ exports.signIn = (email, password, result) => {
             console.log("HANDLER ERROR ? ", error);
             utils.handleError(error, result);
         });
-=======
-  userModel
-    .findOne({ where: { email: email } })
-    .then((us) => {
-      if (us && us.password === password) {
-        result(null, { us });
-      } else {
-        result({ message: "User or password incorrect" }, null);
-      }
-    })
-    .catch((error) => {
-      utils.handleError(error, result);
-    });
->>>>>>> Se agrega logica para el signIn
 };
 
 exports.create = (req, result) => {
@@ -45,7 +30,6 @@ exports.create = (req, result) => {
         });
 };
 exports.signUp = (req, result) => {
-<<<<<<< HEAD
     const userCreate = createUserModel(req);
     userCreate.active_user = 0;
     userCreate.id_role = 1;
@@ -57,20 +41,6 @@ exports.signUp = (req, result) => {
         .catch((error) => {
             utils.handleError(error, result);
         });
-=======
-  const userCreate = createUserModel(req);
-  userCreate.active_user = 0;
-  userCreate.id_role = 1;
-
-  userModel
-    .create(userCreate)
-    .then((newUser) => {
-      result(null, newUser);
-    })
-    .catch((error) => {
-      utils.handleError(error, result);
-    });
->>>>>>> Se agrega logica para el signIn
 };
 
 exports.getAll = (result) => {
