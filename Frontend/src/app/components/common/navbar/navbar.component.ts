6bcb4403ed0f141ebf968edc8a65b21699df2610
rbt226 <robert226@gmail.com>
2020-05-10 04:15:45 -0300

    navbar - username & image

--- Frontend/src/app/components/common/navbar/navbar.component.ts
+++ Frontend/src/app/components/common/navbar/navbar.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { ModalService } from 'src/app/services/modal.service';
 
-
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-    constructor(public authService: AuthService, private modalService: ModalService) { }
+    constructor(
+        public authService: AuthService,
+        private modalService: ModalService
+    ) {}
 
     public isMenuCollapsed = true;
 
--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,20 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../models/user.interface';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService {
     private URL = 'http://localhost:3300/users';
+    public userName: any;
 
-    constructor(private http: HttpClient, private router: Router) { }
+    constructor(private http: HttpClient, private router: Router) {}
 
     signUp(user) {
         return this.http.post<any>(this.URL + '/signUp', user);
     }
     signIn(user) {
+        this.http.post<User>(this.URL + '/byEmail', user).subscribe((res) => {
+            localStorage.setItem('userName', res.user_name);
+            //localStorage.setItem('userImg', res.image_user);
+            localStorage.setItem('userImg', "Capture_v3zpxe");
+        });
         return this.http.post<any>(this.URL + '/signIn', user);
+    }
+
+    getUserName() {
+        return localStorage.getItem('userName');
+    }
+    getUserImg() {
+        return localStorage.getItem('userImg');
     }
 
     loggedIn(): boolean {
@@ -31,6 +45,8 @@ export class AuthService {
 
     logout() {
         localStorage.removeItem('token');
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userImg');
         this.router.navigate(['/']);
     }
 }
