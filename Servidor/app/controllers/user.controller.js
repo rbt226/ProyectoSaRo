849bd33353c1dcce911e40274afa00e8824d4cdd
Sabrina Sosa <ssosa@technisys.com>
2020-05-10 04:51:48 -0300

    Se cambia var por consst

--- Servidor/app/controllers/user.controller.js
+++ Servidor/app/controllers/user.controller.js
@@ -1,39 +1,47 @@
 const userDao = require("../dao/user.dao");
 const clientDao = require("../dao/client.dao");
-var cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary").v2;
 const IncomingForm = require("formidable").IncomingForm;
-var fs = require("fs");
-var FileReader = require("filereader");
-
-// // cloudinary configuration
-// cloudinary.config({
-//   cloud_name: "djbmfd9y6",
-//   api_key: "771838748496195",
-//   api_secret: "yn9HS_biy7UuFGtTZVxhIytA7kg",
-// });
-
-var jwt = require("jsonwebtoken");
+const FileReader = require("filereader");
+const jwt = require("jsonwebtoken");
 
 exports.signIn = (req, res) => {
-    // Validate request
-    if (!req.body) {
-        res.status(400).send({
-            error: { message: "Content can not be empty!" },
-        });
-    }
-    const email = req.body.email;
-    const password = req.body.password;
-    userDao.signIn(email, password, (error, data) => {
-        if (error)
-            res.status(500).send({
-                error,
-            });
-        else {
-            const token = jwt.sign({ _id: data.id_user }, "secretKey");
-            res.send({ token, data });
-        }
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      error: { message: "Content can not be empty!" },
     });
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  userDao.signIn(email, password, (error, data) => {
+    if (error)
+      res.status(500).send({
+        error,
+      });
+    else {
+      const token = jwt.sign({ _id: data.id_user }, "secretKey");
+      res.send({ token, data });
+    }
+  });
 };
+
+// console.log("LLamo a cloudinary ", JSON.stringify(cloudinary));
+// cloudinary.api.delete_resources(
+//   "uciuilekbmhwlzzjfvyf",
+//   { invalidate: true, resource_type: "image" },
+//   function (err, res) {
+//     if (err) {
+//       console.log("error en cloudninary :", err);
+//       return res.status(400).json({
+//         ok: false,
+//         menssage: "Error deleting file",
+//         errors: err,
+//       });
+//     }
+//     console.log("respuesta", res);
+//   }
+// );
 
 exports.signUp = (req, res) => {
   // Validate request
@@ -43,26 +51,7 @@ exports.signUp = (req, res) => {
     });
   }
 
-  // console.log("LLamo a cloudinary ", JSON.stringify(cloudinary));
-  // cloudinary.api.delete_resources(
-  //   "uciuilekbmhwlzzjfvyf",
-  //   { invalidate: true, resource_type: "image" },
-  //   function (err, res) {
-  //     if (err) {
-  //       console.log("error en cloudninary :", err);
-  //       return res.status(400).json({
-  //         ok: false,
-  //         menssage: "Error deleting file",
-  //         errors: err,
-  //       });
-  //     }
-  //     console.log("respuesta", res);
-  //   }
-  // );
-  // Somewhere at the start of your file
-
-  // ...
-  var form = new IncomingForm();
+  const form = new IncomingForm();
   let defaultImage = true;
   let file;
   form.parse(req, function (err, fields, files) {
@@ -153,128 +142,128 @@ exports.signUp = (req, res) => {
 };
 
 exports.create = (req, res) => {
-    // Validate request
-    if (!req.body) {
-        res.status(400).send({
-            error: { message: "Content can not be empty!" },
-        });
-    }
-
-    // Save User in the database
-    userDao.create(req, (error, data) => {
-        if (error)
-            res.status(500).send({
-                error,
-            });
-        else res.send(data);
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      error: { message: "Content can not be empty!" },
     });
+  }
+
+  // Save User in the database
+  userDao.create(req, (error, data) => {
+    if (error)
+      res.status(500).send({
+        error,
+      });
+    else res.send(data);
+  });
 };
 
 // Retrieve all users from the database.
 exports.getAll = (req, res) => {
-    userDao.getAll((error, data) => {
-        if (error)
-            res.status(500).send({
-                error,
-            });
-        else res.send(data);
-    });
+  userDao.getAll((error, data) => {
+    if (error)
+      res.status(500).send({
+        error,
+      });
+    else res.send(data);
+  });
 };
 
 exports.getUserById = (req, res) => {
-    userDao.getUserById(req.params.id, (error, data) => {
-        if (error) {
-            if (error.kind === "not_found") {
-                res.status(404).send({
-                    error: {
-                        message: `No se encontro user con el identificador ${req.params.id}.`,
-                    },
-                });
-            } else {
-                res.status(500).send({
-                    error,
-                });
-            }
-        } else res.send(data);
-    });
+  userDao.getUserById(req.params.id, (error, data) => {
+    if (error) {
+      if (error.kind === "not_found") {
+        res.status(404).send({
+          error: {
+            message: `No se encontro user con el identificador ${req.params.id}.`,
+          },
+        });
+      } else {
+        res.status(500).send({
+          error,
+        });
+      }
+    } else res.send(data);
+  });
 };
 
 exports.deleteById = (req, res) => {
-    userDao.deleteById(req.params.id, (error, data) => {
-        if (error) {
-            if (error.kind === "not_found") {
-                res.status(404).send({
-                    error: {
-                        message: `No se encontro user con el identificador ${req.params.id}.`,
-                    },
-                });
-            } else {
-                res.status(500).send({
-                    error,
-                });
-            }
-        } else res.send({ message: `user was deleted successfully!` });
-    });
+  userDao.deleteById(req.params.id, (error, data) => {
+    if (error) {
+      if (error.kind === "not_found") {
+        res.status(404).send({
+          error: {
+            message: `No se encontro user con el identificador ${req.params.id}.`,
+          },
+        });
+      } else {
+        res.status(500).send({
+          error,
+        });
+      }
+    } else res.send({ message: `user was deleted successfully!` });
+  });
 };
 
 exports.deleteAll = (req, res) => {
-    userDao.deleteAll((error, data) => {
-        if (error)
-            res.status(500).send({
-                error,
-            });
-        else
-            res.send({ message: `All users were deleted successfully! - ${data}` });
-    });
+  userDao.deleteAll((error, data) => {
+    if (error)
+      res.status(500).send({
+        error,
+      });
+    else
+      res.send({ message: `All users were deleted successfully! - ${data}` });
+  });
 };
 
 exports.update = (req, res) => {
-    // Validate Request
-    if (!req.body) {
-        res.status(400).send({
-            error: { message: "Content can not be empty!" },
-        });
-    }
-
-    userDao.updateById(req.params.id, req, (error, data) => {
-        if (error) {
-            if (error.kind === "not_found") {
-                res.status(404).send({
-                    error: {
-                        message: `No se encontro User con el identificador ${req.params.id}.`,
-                    },
-                });
-            } else {
-                res.status(500).send({
-                    error,
-                });
-            }
-        } else res.send({ message: `user was updated successfully!` });
+  // Validate Request
+  if (!req.body) {
+    res.status(400).send({
+      error: { message: "Content can not be empty!" },
     });
+  }
+
+  userDao.updateById(req.params.id, req, (error, data) => {
+    if (error) {
+      if (error.kind === "not_found") {
+        res.status(404).send({
+          error: {
+            message: `No se encontro User con el identificador ${req.params.id}.`,
+          },
+        });
+      } else {
+        res.status(500).send({
+          error,
+        });
+      }
+    } else res.send({ message: `user was updated successfully!` });
+  });
 };
 
 exports.getUserByEmail = (req, res) => {
-    // Validate request
-    if (!req.body) {
-        res.status(400).send({
-            error: { message: "Content can not be empty!" },
-        });
-    }
-    console.log("Controller body : ", JSON.stringify(req.body));
-    const email = req.body.email;
-    userDao.getUserByEmail(email, (error, data) => {
-        if (error) {
-            if (error.kind === "not_found") {
-                res.status(404).send({
-                    error: { message: `No se encontro usuario con email:  ${email}.` },
-                });
-            } else {
-                res.status(500).send({
-                    error,
-                });
-            }
-        } else {
-            res.send(data);
-        }
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      error: { message: "Content can not be empty!" },
     });
+  }
+  console.log("Controller body : ", JSON.stringify(req.body));
+  const email = req.body.email;
+  userDao.getUserByEmail(email, (error, data) => {
+    if (error) {
+      if (error.kind === "not_found") {
+        res.status(404).send({
+          error: { message: `No se encontro usuario con email:  ${email}.` },
+        });
+      } else {
+        res.status(500).send({
+          error,
+        });
+      }
+    } else {
+      res.send(data);
+    }
+  });
 };
