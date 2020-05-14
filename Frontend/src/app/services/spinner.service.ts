3b3ec36e1ece150202291470ed67582230d88841
Sabrina Sosa <ssosa@technisys.com>
2020-05-13 23:21:19 -0300

    Se agrega el spinner por encima del modal por lo que no se necesita bloquear boton de send.

--- Frontend/src/app/services/spinner.service.ts
+++ Frontend/src/app/services/spinner.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class SpinnerService {
-  constructor(public spinner: NgxSpinnerService) {}
-  showSpinner() {
-    this.spinner.show();
-  }
-  hideSpinner() {
-    this.spinner.hide();
-  }
+    constructor(public spinner: NgxSpinnerService) { }
+    showSpinner() {
+        this.spinner.show();
+    }
+    hideSpinner() {
+        this.spinner.hide();
+    }
 }
