6747e11b1593e58d608b501ad87ef6f4b1204b0a
Sabrina Sosa <ssosa@technisys.com>
2020-07-22 23:43:05 -0300

    Se cambia la identacion a 4 tabs.

--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -3,22 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Room } from '../models/room.interface';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class RoomService {
-  private URL = 'http://localhost:3300/rooms/';
+	private URL = 'https://consultorios-del-parque-server.herokuapp.com//rooms/';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) { }
 
-  getRooms() {
-    return this.http.get<any>(this.URL);
-  }
+	getRooms() {
+		return this.http.get<any>(this.URL);
+	}
 
-  getRoomById(idRoom) {
-    return this.http.get<any>(this.URL + idRoom);
-  }
+	getRoomById(idRoom) {
+		return this.http.get<any>(this.URL + idRoom);
+	}
 
-  createRoom(room) {
-    return this.http.post<any>(this.URL, room);
-  }
+	createRoom(room) {
+		return this.http.post<any>(this.URL, room);
+	}
 }
