53a275d9efdc626e0c717816af464663add71551
Sabrina Sosa <ssosa@technisys.com>
2020-07-22 23:43:45 -0300

    Se cambia el Procfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + "/dist/Frontend"));
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/Frontend/index.html"));
+	res.sendFile(path.join(__dirname + "/dist/Frontend/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
