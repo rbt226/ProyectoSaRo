02563526e31192ddef8c778c7063b42283dcb3a9
rbt226 <robert226@gmail.com>
2020-07-24 22:31:03 -0300

    cambiar contraseña

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -10,79 +10,78 @@ import { UserService } from 'src/app/services/user.service';
 import Utils from '../../utils/utils';
 
 @Component({
-    selector: 'app-sign-in',
-    templateUrl: './sign-in.component.html',
-    styleUrls: ['./sign-in.component.scss'],
+	selector: 'app-sign-in',
+	templateUrl: './sign-in.component.html',
+	styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-    constructor(
-        private autService: AuthService,
-        private formBuilder: FormBuilder,
-        private router: Router,
-        private spinnerService: SpinnerService,
-        public modalService: ModalService,
-        private emailService: EmailService,
-        private notification: NotificationService,
-        private usrService: UserService
-    ) { }
+	constructor(
+		private autService: AuthService,
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private spinnerService: SpinnerService,
+		public modalService: ModalService,
+		private emailService: EmailService,
+		private notification: NotificationService,
+		private usrService: UserService
+	) {}
 
-    formSignIn: FormGroup;
-    formSubmitted = false;
-    forgotPass = false;
+	formSignIn: FormGroup;
+	formSubmitted = false;
+	forgotPass = false;
 
-    ngOnInit() {
-        this.formSignIn = this.formBuilder.group({
-            email: ['', [Validators.pattern('^[^@]+@[^@]+\\.[a-zA-Z]{2,}$')]],
-            password: ['', Validators.required],
-        });
-    }
+	ngOnInit() {
+		this.formSignIn = this.formBuilder.group({
+			email: ['', [Validators.pattern('^[^@]+@[^@]+\\.[a-zA-Z]{2,}$')]],
+			password: ['', Validators.required],
+		});
+	}
 
-    get form() {
-        return this.formSignIn.controls;
-    }
+	get form() {
+		return this.formSignIn.controls;
+	}
 
-    signIn(data) {
-        event.preventDefault();
-        this.formSubmitted = true;
-        if (!this.forgotPass) {
-            if (this.formSignIn.valid) {
-                this.spinnerService.showSpinner();
+	signIn(data) {
+		event.preventDefault();
+		this.formSubmitted = true;
+		if (!this.forgotPass) {
+			if (this.formSignIn.valid) {
+				this.spinnerService.showSpinner();
 
-                this.autService.signIn(data).subscribe((res) => {
-                    this.spinnerService.hideSpinner();
-                    if (Utils.isOkResponse(res)) {
-                        this.usrService.setUserData(res.data.user_name, res.data.image_user);
-                        localStorage.setItem('token', res.token);
-                        this.router.navigate(['/']);
-                        this.formSignIn.reset();
-                        this.modalService.hideModal();
-                    } else {
-                        this.notification.showError(res.message);
-                    }
-                });
-            }
-        } else {
-            this.spinnerService.showSpinner();
-            this.usrService.getUserByEmail(data).subscribe((res) => {
-                if (Utils.isOkResponse(res)) {
-                    this.emailService
-                        .sendEmail({
-                            action: 'forgot',
-                            to: data.email,
-                        })
-                        .subscribe((resEmail) => {
-                            this.spinnerService.hideSpinner();
-                            this.notification.showSuccess(resEmail.message);
-                            this.router.navigate(['/']);
-                            this.formSignIn.reset();
-                            this.modalService.hideModal();
-                        });
-                }
-                else {
-                    this.spinnerService.hideSpinner();
-                    this.notification.showError(res.message);
-                }
-            });
-        }
-    }
+				this.autService.signIn(data).subscribe((res) => {
+					this.spinnerService.hideSpinner();
+					if (Utils.isOkResponse(res)) {
+						this.usrService.setUserData(res.data.user_name, res.data.image_user);
+						localStorage.setItem('token', res.token);
+						this.router.navigate(['/']);
+						this.formSignIn.reset();
+						this.modalService.hideModal();
+					} else {
+						this.notification.showError(res.message);
+					}
+				});
+			}
+		} else {
+			this.spinnerService.showSpinner();
+			this.usrService.getUserByEmail(data).subscribe((res) => {
+				if (Utils.isOkResponse(res)) {
+					this.emailService
+						.sendEmail({
+							action: 'forgot',
+							to: data.email,
+						})
+						.subscribe((resEmail) => {
+							this.spinnerService.hideSpinner();
+							this.notification.showSuccess(resEmail.message);
+							this.router.navigate(['/']);
+							this.formSignIn.reset();
+							this.modalService.hideModal();
+						});
+				} else {
+					this.spinnerService.hideSpinner();
+					this.notification.showError(res.message);
+				}
+			});
+		}
+	}
 }
